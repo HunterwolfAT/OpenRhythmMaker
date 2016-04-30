13f733141bdd0cbda13a0ad8fa489837b4f7c882
HunterwolfAT <hunterwolf0815@gmail.com>
2016-04-30 13:17:43 +0200

    made script sync with music (hacky tho)

--- rhythmdata.gd
+++ rhythmdata.gd
@@ -1,33 +1,64 @@
 
 extends Position2D
 
-# member variables here, example:
-# var a=2
-# var b="textvar"
 var acc = 0
-var bpm = 55.0 #undertale song has 110 BPM
-var tolerance = 0.6
+var bpm = 270.0 # 270 for hole in one
+var tolerance = 0.3
 var limit = 60.0 / bpm
 var beatplayed = false
-var animoffset = 0.7
+var animoffset = 0.0
+var offset = 1 #1 for holeinone
+var offsetover = false
+
+var beats = 0
+var hit = 0
+var fail = 0
+var missed = 0
+
+var hitframe = false #the timefrime when a input is registered as a hit
+var washit = false
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	set_process(true)
+	set_process_input(true)
 	pass
 
 func _process(delta):
 	acc += delta
-	if (acc > 1 && !beatplayed):
-		get_node("Position2D/big").beat()
-		get_node("Position2D 2/smol").beat()
-		beatplayed = true;
-	if (acc > 1.1):
-		get_node("Position2D/big").set_modulate(Color(0.5, 1.0, 0.5)) # make green
-	if (acc > 1.4):
-		get_node("Position2D/big").set_modulate(Color(1, 1, 1)) # make normal
-		acc = 0
-		beatplayed = false;
+	if (!offsetover):
+		if (acc >= offset):
+			offsetover = true
+			acc = 0
+	else:
+		if (acc >= limit - animoffset && !beatplayed):
+			get_node("Position2D/big").beat()
+			get_node("Position2D 2/smol").beat()
+			beatplayed = true;
+		if (acc >= limit - animoffset):
+			get_node("Position2D/big").set_modulate(Color(0.5, 1.0, 0.5)) # make green
+			hitframe = true
+		if (acc >= limit + tolerance - animoffset):
+			get_node("Position2D/big").set_modulate(Color(1, 1, 1)) # make normal
+			acc -= limit + tolerance
+			beatplayed = false;
+			beats += 1
+			if (!washit):
+				missed += 1
+			washit = false
+			hitframe = false
+			get_node("L_Beats").set_text("Beats: " + str(beats) + " | Hit: " + str(hit) + " | Wrong: " +str(missed + fail) )
 
-
+func _input(event):
+	if (event.type == InputEvent.KEY):
+		if (Input.is_key_pressed(KEY_SPACE)):
+			if (hitframe):
+				hit += 1
+				washit = true
+			else:
+				fail += 1
+		
+		#Quit the game
+		if (Input.is_key_pressed(KEY_ESCAPE)):
+			get_tree().quit()
