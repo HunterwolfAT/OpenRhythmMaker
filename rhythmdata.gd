92f3a2b071b1f19fd4c0c032d590bada8dede015
HunterwolfAT <hunterwolf0815@gmail.com>
2016-05-01 08:57:53 +0200

    BPM work now

--- rhythmdata.gd
+++ rhythmdata.gd
@@ -2,12 +2,12 @@
 extends Position2D
 
 var acc = 0
-var bpm = 270.0 # 270 for hole in one
-var tolerance = 0.3
+var totalacc = 0
+var bpm = 110.0 # 115 for hole in one
+var tolerance = 0.2
 var limit = 60.0 / bpm
 var beatplayed = false
-var animoffset = 0.0
-var offset = 1 #1 for holeinone
+var offset = 0.3 #1 for holeinone
 var offsetover = false
 
 var beats = 0
@@ -27,21 +27,22 @@ func _ready():
 
 func _process(delta):
 	acc += delta
+	totalacc += delta
 	if (!offsetover):
 		if (acc >= offset):
 			offsetover = true
 			acc = 0
 	else:
-		if (acc >= limit - animoffset && !beatplayed):
+		if (acc >= limit && !beatplayed):
 			get_node("Position2D/big").beat()
 			get_node("Position2D 2/smol").beat()
 			beatplayed = true;
-		if (acc >= limit - animoffset):
+		if (acc >= limit):
 			get_node("Position2D/big").set_modulate(Color(0.5, 1.0, 0.5)) # make green
 			hitframe = true
-		if (acc >= limit + tolerance - animoffset):
+		if (acc >= limit + tolerance):
 			get_node("Position2D/big").set_modulate(Color(1, 1, 1)) # make normal
-			acc -= limit + tolerance
+			acc -= limit
 			beatplayed = false;
 			beats += 1
 			if (!washit):
@@ -49,6 +50,7 @@ func _process(delta):
 			washit = false
 			hitframe = false
 			get_node("L_Beats").set_text("Beats: " + str(beats) + " | Hit: " + str(hit) + " | Wrong: " +str(missed + fail) )
+		print(str(totalacc) + " | " + str(acc))
 
 func _input(event):
 	if (event.type == InputEvent.KEY):
